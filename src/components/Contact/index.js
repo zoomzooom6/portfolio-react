4a5c6b0683a3ec6fdbc052308dc858387752d492
zoomzoom6 <tonyhuang107@live.ca>
2021-09-06 15:24:17 -0400

    setup contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
-
+            <h1>Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </section>
     );
 }
