e2543ce1d8f7894d8abec0b100ca1955bff42e29
zoomzoom6 <tonyhuang107@live.ca>
2021-09-23 18:34:47 -0400

    add titles to social media anchor tags

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ function Footer() {
     return (
         <footer>
             <div className="container">
-                <a href="https://www.linkedin.com/in/tonyhsiaotunghuang/" class="fa fa-linkedin" rel="noreferrer" target="_blank"></a>
-                <a href="https://github.com/zoomzooom6" class="fa fa-github" rel="noreferrer" target="_blank"></a>
-                <a href="https://twitter.com/thth107" class="fa fa-twitter" rel="noreferrer" target="_blank"></a>
+                <a href="https://www.linkedin.com/in/tonyhsiaotunghuang/" class="fa fa-linkedin" rel="noreferrer" target="_blank" title="Connect with me on LinkedIn"></a>
+                <a href="https://github.com/zoomzooom6" class="fa fa-github" rel="noreferrer" target="_blank" title="My GitHub Profile"></a>
+                <a href="https://twitter.com/thth107" class="fa fa-twitter" rel="noreferrer" target="_blank" title="Follow me on Twitter"></a>
             </div>
         </footer>
     );
